d6868ed4cf1e2268ac0f0147a917f7ce6d58ffbf
Usmonbek <usmonravshanov@list.ru>
2025-01-15 10:33:12 +0500

    minor

--- app/main.py
+++ app/main.py
@@ -24,6 +24,5 @@ app.add_middleware(
     allow_headers=["*"],  # Allow all headers (e.g., Authorization, Content-Type)
 )
 
-
 if __name__ == "__main__":
     uvicorn.run("app.main:app", host="0.0.0.0", port=8001, workers=1)
